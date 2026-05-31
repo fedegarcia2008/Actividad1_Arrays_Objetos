4d449958f4993341cb13b096a81fcb2b9fcc40a7
fedegarcia2008 <garcia.federico@tecnica7.edu.ar>
2026-05-31 19:22:22 -0300

    Recorrer array

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ document.getElementById('ing').addEventListener('click', () => {
     datos[0].ant = parseInt(document.getElementById('a').value);
 
     datos.forEach(empleado => {
+        if (empleado.cat == "A") {
+            empleado.bruto = 1840;
+        } else if (empleado.cat == "B") {
+            empleado.bruto = 1080;
+        } else {
+            empleado.bruto = 2000;
+        }
+
+        empleado.jub = ((empleado.bruto * 11) / 100);
+        
+        empleado.sind = ((empleado.bruto * 4) / 100);
+        
+        empleado.obra = ((empleado.bruto * 3) / 100);
+        
+        if (empleado.ant > 17) {
+            empleado.premio = 100;
+        }
+        
+        empleado.neto = ((empleado.bruto + empleado.premio) - (empleado.jub + empleado.sind + empleado.obra));
+
         console.log(empleado);
     });
 
