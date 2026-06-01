12fbaefbee15563f120298735290647723324195
fedegarcia2008 <garcia.federico@tecnica7.edu.ar>
2026-06-01 03:04:33 -0300

    Creación de tablas

--- script.js
+++ script.js
@@ -33,19 +33,108 @@ document.getElementById('ing').addEventListener('click', () => {
     empleado.neto = (empleado.bruto + empleado.premio) - (empleado.jub + empleado.sind + empleado.obra);
 
     datos.push(empleado);
-    console.log(empleado);
     
 });
 
 document.getElementById('ver').addEventListener('click', () => {
+    salida.innerHTML = '';
     function mostrarEmpleados() {
-        salida.innerHTML = '';
-        let categC = 0; let cantA = 0; let cantB = 0; let cantC = 0;
         
-            //tabla de liquidacion
+       // let categC = 0; 
+        let cantA = 0; let cantB = 0; let cantC = 0;
+        //tabla de liquidacion
 
-            let tabla = document.createElement('table');
-            tabla.innerHTML = `
+        let tabla = document.createElement('table');
+
+        tabla.innerHTML = `
+            <tr>
+            <th>EMPLEADO</th>
+            <th>CATEGORÍA</th>
+            <th>ANTIGÜEDAD</th>
+            <th>SUELDO BRUTO</th>
+            <th>JUBILACIÓN</th>
+            <th>SINDICATO</th>
+            <th>OBRA SOCIAL</th>
+            <th>PREMIO</th>
+            <th>SUELDO NETO</th>
+            </tr>
+             `;
+             
+             datos.forEach(empleado => {
+
+            if (empleado.cat == "A") {
+                cantA++;
+            } else if (empleado.cat == "B") {
+                cantB++;
+            } else {
+             //   categC += empleado.neto;
+                cantC++;
+            }
+            
+            let fila = document.createElement('tr');
+            fila.innerHTML = `
+            <td>${empleado.nom}</td>
+            <td>${empleado.cat}</td>
+            <td>${empleado.ant} años</td>
+            <td>$${empleado.bruto}</td>
+            <td>$${empleado.jub}</td>
+            <td>$${empleado.sind.toFixed(2)}</td>
+            <td>$${empleado.obra.toFixed(2)}</td>
+            <td>$${empleado.premio}</td>
+            <td>$${empleado.neto}</td>
+            `;
+
+            if (empleado.ant < 15) {
+                fila.style.color = "red";
+    } else if (empleado.ant > 20) {
+        fila.style.color = "blue";
+    }
+
+    tabla.appendChild(fila);
+
+});
+
+salida.appendChild(tabla);
+    }
+    mostrarEmpleados();
+
+    function netoC() {
+
+        let tabla = document.createElement('table');
+        let h2 = document.createElement('h2');
+        h2.textContent = `Sueldo Neto - Categoría C`;
+        tabla.innerHTML = `
+            <tr>
+            <th>EMPLEADO</th>
+            <th>CATEGORÍA</th>
+            <th>SUELDO NETO</th>
+            </tr>
+             `;
+
+        datos.forEach(empleado => {
+            if (empleado.cat == "C") {
+                        let fila = document.createElement('tr');
+            fila.innerHTML = `
+            <td>${empleado.nom}</td>
+            <td>${empleado.cat}</td>
+            <td>$${empleado.neto}</td>
+            `;
+            tabla.appendChild(fila);
+                }
+            });
+            salida.appendChild(h2);
+            salida.appendChild(tabla);
+            
+    }
+    netoC();
+
+
+        function liquidacionA() {
+        
+        let tabla = document.createElement('table');
+        let h2 = document.createElement('h2');
+        h2.textContent = `Liquidación - Categoría A`;
+        tabla.innerHTML = `
             <tr>
             <th>EMPLEADO</th>
             <th>CATEGORÍA</th>
@@ -60,41 +149,83 @@ document.getElementById('ver').addEventListener('click', () => {
              `;
 
         datos.forEach(empleado => {
-
             if (empleado.cat == "A") {
-                cantA++;
-            } else if (empleado.cat == "B") {
-                cantB++;
-            } else {
-                categC += empleado.neto;
-                cantC++;
-            }
+                        let fila = document.createElement('tr');
+            fila.innerHTML = `
+            <td>${empleado.nom}</td>
+            <td>${empleado.cat}</td>
+            <td>${empleado.ant} años</td>
+            <td>$${empleado.bruto}</td>
+            <td>$${empleado.jub}</td>
+            <td>$${empleado.sind.toFixed(2)}</td>
+            <td>$${empleado.obra.toFixed(2)}</td>
+            <td>$${empleado.premio}</td>
+            <td>$${empleado.neto}</td>
+            `;
+            tabla.appendChild(fila);
+                }
+            });
+            salida.appendChild(h2);
+            salida.appendChild(tabla);
             
-    let fila = document.createElement('tr');
-
-    fila.innerHTML = `
-        <td>${empleado.nom}</td>
-        <td>${empleado.cat}</td>
-        <td>${empleado.ant}</td>
-        <td>${empleado.bruto}</td>
-        <td>${empleado.jub}</td>
-        <td>${empleado.sind}</td>
-        <td>${empleado.obra}</td>
-        <td>${empleado.premio}</td>
-        <td>${empleado.neto}</td>
-    `;
-
-    tabla.appendChild(fila);
-
-
-        });
-
-salida.appendChild(tabla);
-        //DATOS A MOSTRAR
-        let h2 = document.createElement('h1');
-        h2.textContent = `Neto Categoría C: $${categC} - CantA: ${cantA} - CantB: ${cantB} - CantC: ${cantC}`;
-        salida.appendChild(h2);
-
     }
-    mostrarEmpleados();
+    liquidacionA();
+
+    function neto12() {
+
+        let tabla = document.createElement('table');
+        let h2 = document.createElement('h2');
+        h2.textContent = `Empleados - Sueldo neto mayor a $1200`;
+        tabla.innerHTML = `
+            <tr>
+            <th>EMPLEADO</th>
+            <th>SUELDO NETO</th>
+            </tr>
+             `;
+
+        datos.forEach(empleado => {
+            if (empleado.neto > 1200) {
+                        let fila = document.createElement('tr');
+            fila.innerHTML = `
+            <td>${empleado.nom}</td>
+            <td>$${empleado.neto}</td>
+            `;
+            tabla.appendChild(fila);
+                }
+            });
+            salida.appendChild(h2);
+            salida.appendChild(tabla);
+            
+    }
+    neto12();
+
+        function antMayor() {
+
+        let tabla = document.createElement('table');
+        let h2 = document.createElement('h2');
+        h2.textContent = `Empleados con más de 6 años de antiguedad`;
+        tabla.innerHTML = `
+            <tr>
+            <th>EMPLEADO</th>
+            <th>ANTIGÜEDAD</th>
+            </tr>
+             `;
+
+        datos.forEach(empleado => {
+            if (empleado.ant > 6) {
+                        let fila = document.createElement('tr');
+            fila.innerHTML = `
+            <td>${empleado.nom}</td>
+            <td>${empleado.ant} años</td>
+            `;
+            tabla.appendChild(fila);
+                }
+            });
+            salida.appendChild(h2);
+            salida.appendChild(tabla);
+            
+    }
+    antMayor();
+
+
 });
